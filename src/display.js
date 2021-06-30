593d0b4e3760b153d3f97f603cff4c08f4b64620
poswerf23343 <resfow2938@gmail.com>
2021-06-30 14:08:37 +0900

    main.jsとalgorithm.jsを追加。 棒の数や棒の大きさをグローバル変数にした。

--- src/display.js
+++ src/display.js
@@ -1,14 +1,8 @@
-let n=10;    //要素数
+
 let a=document.getElementById('squares');   //追加する親
 let b=new Array(n); //四角形のノードたち
-let c=new Array(n); //棒の大きさ
-let d="Array={";    //サイトに表示する棒の大きさ
 
-//*棒の大きさを乱数生成
-for (let i = 0; i < n; i++) {
-    //!0の値だと棒が表示されないので、ceil・・・切り上げを使用。
-    c[i]=Math.ceil(Math.random()*10);
-}
+let d="Array={";    //サイトに表示する棒の大きさ
 
 //*サイトに配列の値を表示する
 for (let i = 0; i < n-1; i++) {

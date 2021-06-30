69aeb0245723222d157d8a2a83a9370175214ea4
silvietf <silwekis3095@gmail.com>
2021-06-30 17:26:03 +0900

    棒を１個動かせるようになる。 （animejs使い始め)

--- src/animation.js
+++ src/animation.js
@@ -1,5 +1,9 @@
 /*ソートアルゴリズムで棒を操作。*/
-//*今回はバブルソートのみ
+//*今回はバブルソートのみ。→なるべく拡張性を持たせたい=sort_filesの中にあるプログラムを選択できるようにしたい。
 function change(element,rgbcolor) {
     element.style.backgroundColor=rgbcolor;
 };
+anime({
+    targets:"#square1",
+    translateY:150
+});

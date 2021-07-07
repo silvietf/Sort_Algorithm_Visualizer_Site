bd12f902867bee2692799256b0401ad79ac6bf4b
silvietf <silwekis3095@gmail.com>
2021-07-07 22:22:13 +0900

    ・バブルソート実装！ ・index_bubble.htmlのbubble.jsのリンクとindez_bubble/htmlへのリンク修正

--- src/sort_files/bubble.js
+++ src/sort_files/bubble.js
@@ -5,40 +5,35 @@
  * !setIntervalの中にsetIntervalは使えない。
  */
 const stick_animation_bubble = async () => {
-    for (let i = 0; i < n; i++) {
-        //赤色表示
-        let me = document.getElementById('square' + String(i + 1));
-        change_color(me, '#dc143c');
-        for (let j = i + 1; j < n; j++) {
+    for (let i = 0; i < n-1; i++) {
+        for (let j = 0; j < n-1; j++) {
+            //赤色表示
+            let me = document.getElementById('square' + String(j + 1));
+            change_color(me, '#dc143c');
             //青棒表示
-            let target = document.getElementById('square' + String(j + 1));
-            //!await wait(100)をここに挟むと色blue⇒black切り替えが一瞬で行われるので注意！！
+            let target = document.getElementById('square' + String(j + 2));
             change_color(target, '#00bfff');
-            await wait(500);
-            console.log(me);
-            if (c[i] > c[j]) {
-
+            await wait(250);
+            if (c[j] > c[j+1]) {
                 change_color(me, '#00ff00');
                 change_color(target, '#00ff00');
-                change_array(i, j);
+                change_array(j, j+1);
                 await wait(250);
+
                 change_stick(me, target);
                 await wait(250);
+
                 change_color(me, '#dc143c');
                 change_color(target, '#00bfff');
-
             }
-            change_color(target, '#00bfff');
+            change_color(me,'#000000');
+            change_color(target,'#000000');
         }
-        if (i == n - 1) {
-            //!i==n-1のときにはfor(let j=0; ....){}を除外してしまう。→await wait(200)で赤色の棒を人が認識できるように。
-            await wait(500);
-        }
-        for (let k = 0; k < n; k++) {
-            //色の初期化
-            let clear = document.getElementById('square' + String(k + 1));
-            change_color(clear, '#000000');
-        }
+    }
+    for (let k = 0; k < n; k++) {
+        //色の初期化
+        let clear = document.getElementById('square' + String(k + 1));
+        change_color(clear, '#000000');
     }
 }
 stick_animation_bubble();
